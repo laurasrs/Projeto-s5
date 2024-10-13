243b8454af01d82ffceb7ffe52db85814d0c08c9
Laura Soares <70729015+laurasrs@users.noreply.github.com>
2024-10-13 20:57:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,21 @@ import pandas as pd
 import plotly.express as px
 
 # Carregar o dataset
-df = pd.read_csv('vehicles_us.csv')
+df = pd.read_csv('vehicles.csv')
 
 # Título do aplicativo
 st.header('Análise de Anúncios de Carros')
 
 # Botão para gerar um histograma
-if st.button('Mostrar Histograma de Preços'):
-    fig = px.histogram(df, x='price', nbins=50, title='Distribuição de Preços')
-    st.plotly_chart(fig)
+hist_button = st.button('Criar histograma') # criar um botão
+if hist_button:
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    fig = px.histogram(df, x="odometer", nbins=50)
+    st.plotly_chart(fig, use_container_width=True)
 
 # Botão para gerar um gráfico de dispersão
-if st.button('Mostrar Gráfico de Dispersão (Preço vs. Quilometragem)'):
+disp_button = st.button('Criar Gráfico de Dispersão (Preço vs. Quilometragem)'):
+if disp_button:
+    st.write('Criando um Gráfico de Dispersão (Preço vs. Quilometragem)')
     fig = px.scatter(df, x='odometer', y='price', title='Preço vs. Quilometragem')
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
